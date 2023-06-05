c98d050eb3bc990a5923b9c8e173cd0568c92dc5
Apolosmllb <54141767+Apolosmllb@users.noreply.github.com>
2023-06-05 02:12:36 -0500

    feat: add login form component

--- src/app/public/pages/hero/hero.component.ts
+++ src/app/public/pages/hero/hero.component.ts
@@ -1,6 +1,9 @@
 import {Component} from '@angular/core';
 import {Router} from "@angular/router";
 import {LayoutService} from "src/app/shared/services/layout/layout.service";
+import {DialogService, DynamicDialogRef} from "primeng/dynamicdialog";
+import {LoginFormComponent} from "../../../iam/pages/sign-in/login-form.component";
+import {MessageService} from "primeng/api";
 
 
 @Component({
@@ -9,11 +12,36 @@ import {LayoutService} from "src/app/shared/services/layout/layout.service";
   styleUrls: ['./hero.component.css']
 })
 export class HeroComponent {
-  constructor(public layoutService: LayoutService, public router: Router) {
-  }
-  navigateToHighlights() {
-    console.log('dddd')
-    this.router.navigate(['/public/rca'], { fragment: 'highlights' });
+  visible!: boolean;
+  ref!: DynamicDialogRef;
+
+  constructor(public layoutService: LayoutService,
+              public router: Router,
+              public messageService: MessageService,
+              public dialogService: DialogService) {
   }
 
+  show() {
+    this.ref = this.dialogService.open(LoginFormComponent, {
+      //width: 'calc(100hv- 40px)',
+      contentStyle: { overflow: 'auto' },
+      baseZIndex: 10000,
+
+    });
+
+    this.ref.onClose.subscribe(( ) => {
+        this.messageService.add({ severity: 'info', summary: 'Product Selected'});
+
+    });
+
+    this.ref.onMaximize.subscribe((value) => {
+      this.messageService.add({ severity: 'info', summary: 'Maximized', detail: `maximized: ${value.maximized}` });
+    });
+  }
+
+  ngOnDestroy() {
+    if (this.ref) {
+      this.ref.close();
+    }
+  }
 }
