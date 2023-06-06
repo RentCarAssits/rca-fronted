1041c218e5cd41c952e3e0421b8c511528007ce5
Miner <54141767+Apolosmllb@users.noreply.github.com>
2023-06-06 17:58:49 -0500

    fix : add new view to list car page

--- src/app/renting/pages/car-rental-list/car-rental-list.component.ts
+++ src/app/renting/pages/car-rental-list/car-rental-list.component.ts
@@ -8,11 +8,36 @@ import {HttpClient} from "@angular/common/http";
 })
 export class CarRentalListComponent implements  OnInit{
   cars!: any[];
+  responsiveOptions!: any[];
+
   constructor(private http: HttpClient) { }
+
+
   ngOnInit() {
+
     this.http.get<any[]>('/assets/json/cars.json').subscribe(data => {
       this.cars = data; // Asigna los datos del JSON a la variable cars
       console.log(this.cars); // Puedes ver los datos en la consola
     });
+
+    this.responsiveOptions = [
+      {
+        breakpoint: '1199px',
+        numVisible: 1,
+        numScroll: 1
+      },
+      {
+        breakpoint: '991px',
+        numVisible: 2,
+        numScroll: 1
+      },
+      {
+        breakpoint: '767px',
+        numVisible: 1,
+        numScroll: 1
+      }
+    ];
+
+
   }
 }
