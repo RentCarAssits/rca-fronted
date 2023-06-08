ddef5286353416e9454887b72110be445e9da6ba
XFernando43 <102422211+XFernando43@users.noreply.github.com>
2023-06-08 18:01:19 -0500

    fix: add a better chart grafic

--- src/app/renting/pages/dashboard/dashboard/dashboard.component.ts
+++ src/app/renting/pages/dashboard/dashboard/dashboard.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import {LayoutService} from "../../../../shared/services/layout/layout.service";
 import {MenuItem} from "primeng/api";
 import {CarService} from "../../../services/car/car.service";
@@ -9,6 +9,12 @@ import {CarService} from "../../../services/car/car.service";
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent {
+
+  basicData: any;
+
+  basicOptions: any;
+
+
   items!: MenuItem[];
 
   //products!: Product[];
@@ -20,10 +26,6 @@ export class DashboardComponent {
   subscription!: any;
   totalVehicles!: number;
 
-  data: any;
-
-  options: any;
-
   constructor( public layoutService: LayoutService, private carService: CarService) {
     this.subscription = this.layoutService.configUpdate$.subscribe(() => {
       this.initChart();
@@ -31,10 +33,6 @@ export class DashboardComponent {
   }
 
   ngOnInit() {
-
-    const documentStyle = getComputedStyle(document.documentElement);
-    const textColor = documentStyle.getPropertyValue('--text-color');
-
     this.initChart();
     this.getTotalVehicles();
     this.items = [
@@ -42,28 +40,55 @@ export class DashboardComponent {
       { label: 'Remove', icon: 'pi pi-fw pi-minus' }
     ];
 
-    this.data = {
-      labels: ['A', 'B', 'C'],
+    const documentStyle = getComputedStyle(document.documentElement);
+    const textColor = documentStyle.getPropertyValue('--text-color');
+    const textColorSecondary = documentStyle.getPropertyValue('--text-color-secondary');
+    const surfaceBorder = documentStyle.getPropertyValue('--surface-border');
+
+    this.basicData = {
+      labels: ['Q1', 'Q2', 'Q3', 'Q4'],
       datasets: [
           {
-              data: [5, 12, 20],
-              backgroundColor: [documentStyle.getPropertyValue('--blue-500'), documentStyle.getPropertyValue('--yellow-500'), documentStyle.getPropertyValue('--green-500')],
-              hoverBackgroundColor: [documentStyle.getPropertyValue('--blue-400'), documentStyle.getPropertyValue('--yellow-400'), documentStyle.getPropertyValue('--green-400')]
+              label: 'Sales',
+              data: [540, 325, 702, 620],
+              backgroundColor: ['rgba(255, 159, 64, 0.2)', 'rgba(75, 192, 192, 0.2)', 'rgba(54, 162, 235, 0.2)', 'rgba(153, 102, 255, 0.2)'],
+              borderColor: ['rgb(255, 159, 64)', 'rgb(75, 192, 192)', 'rgb(54, 162, 235)', 'rgb(153, 102, 255)'],
+              borderWidth: 1
           }
-      ]
+        ]
+      };
+
+      this.basicOptions = {
+        plugins: {
+            legend: {
+                labels: {
+                    color: textColor
+                }
+            }
+        },
+        scales: {
+            y: {
+                beginAtZero: true,
+                ticks: {
+                    color: textColorSecondary
+                },
+                grid: {
+                    color: surfaceBorder,
+                    drawBorder: false
+                }
+            },
+            x: {
+                ticks: {
+                    color: textColorSecondary
+                },
+                grid: {
+                    color: surfaceBorder,
+                    drawBorder: false
+                }
+            }
+        }
     };
 
-    this.options = {
-      plugins: {
-          legend: {
-              labels: {
-                  usePointStyle: true,
-                  color: textColor
-              }
-          }
-      }
-  };
-    
 
   }
 
@@ -156,7 +181,3 @@ export class DashboardComponent {
 
   }
 }
-
-
-
-
