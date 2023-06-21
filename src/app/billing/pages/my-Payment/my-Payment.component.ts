4f50b438b2b033aadb205605193e5ce3b6958d64
sebastian Montes <u201712366@upc.edu.pe>
2023-06-21 16:41:27 -0500

    feat: My Payment Component - Initial implementation

---
+++ src/app/billing/pages/my-Payment/my-Payment.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-my-Payment',
+  templateUrl: './my-Payment.component.html',
+  styleUrls: ['./my-Payment.component.css']
+})
+export class MyPaymentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
