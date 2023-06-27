08d5166411124e1e44252d582db585066675a53e
Coltmoons <coltmoonsac@gmail.com>
2023-06-27 17:22:54 -0500

    create table of workshops

--- src/app/workshop/components/workshop/workshop.component.ts
+++ src/app/workshop/components/workshop/workshop.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-workshop',
   templateUrl: './workshop.component.html',
-  styleUrls: ['./workshop.component.css']
+  styleUrls: ['./workshop.component.css'],
 })
-export class WorkshopAComponent {
+export class WorkshopAComponent implements OnInit {
+  // workshops!: any[]
+  workshops = [
+    { id: 1, name: 'hola', ownerId: 3, address: 'la marina' },
+    { id: 2, name: 'hey', ownerId: 4, address: 'la marina' },
+  ];
+  ngOnInit(): void {}
 
+  
 }
