2a893c2b1b88323f785f12a179b83d3ba05b9a0e
Coltmoons <coltmoonsac@gmail.com>
2023-06-28 22:00:46 -0500

    try

--- src/app/workshop/workshop-routing.module.ts
+++ src/app/workshop/workshop-routing.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { WorkshopChatComponent } from './pages/workshop-chat/workshop-chat.component';
+import { WorkshopAComponent } from './components/workshop/workshop.component';
 
 const routes: Routes = [
-    { path: '', redirectTo: '', pathMatch: 'full' },
-    { path: 'chat-box', component: WorkshopChatComponent },
-  
-  ];
-  
-  @NgModule({
-    imports: [RouterModule.forChild(routes)],
-    exports: [RouterModule]
-  })
-  export class WorkshopRoutingModule { }
+  { path: '', redirectTo: '', pathMatch: 'full' },
+  { path: 'chat-box', component: WorkshopChatComponent },
+  { path: 'workshop', component: WorkshopAComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class WorkshopRoutingModule {}
--- src/app/workshop/workshop.module.ts
+++ src/app/workshop/workshop.module.ts
@@ -4,9 +4,11 @@ import { WorkshopChatComponent } from './pages/workshop-chat/workshop-chat.compo
 import { WorkshopRoutingModule } from './workshop-routing.module';
 import { PrimeNgModule } from '../prime-ng/prime-ng.module';
 import {ReactiveFormsModule} from "@angular/forms";
+import { WorkshopAComponent } from './components/workshop/workshop.component';
+import { CreateWorkshopItemComponent } from './components/create-workshop-item/create-workshop-item.component';
 
 @NgModule({
-  declarations: [WorkshopChatComponent],
+  declarations: [WorkshopChatComponent, WorkshopAComponent, CreateWorkshopItemComponent],
   exports: [WorkshopChatComponent],
     imports: [CommonModule, WorkshopRoutingModule, PrimeNgModule, ReactiveFormsModule],
 })
